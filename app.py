73b0db53ca326ca3383c525da080902d26666445
edins19n <edins19n@gmail.com>
2026-06-03 23:30:57 -0500

    Agregando CORS para Vercel

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask_cors import CORS
 import sqlite3
 import os
 
 app = Flask(__name__)
+CORS(app)
 app.secret_key = 'clave_secreta_examen_aplicada'
 
 DATABASE = 'database.db'
